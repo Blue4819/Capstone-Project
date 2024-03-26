0c49179b6e0d176d1dbcefd1c74632a25c644965
Prachi Bharti <prachi.bharti21@st.niituniversity.in>
2024-03-26 15:20:24 +0530

    Update server.js

--- Server Side/server.js
+++ Server Side/server.js
@@ -1,23 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
 
 const userRoutes = require('./routes/user')
 
 //invoking and starting the express app
 const app = express()
+
+//middleware
 app.use(express.json())  
-app.use(cors())
-app.use(cookieParser())
+
 app.use((request, response, next) =>{
     console.log(request.path, request.method)
     next()
 })
 
 //routes
-app.use('/user', userRoutes)
+app.use('/api/user', userRoutes)
 
 //listen for requests
 app.listen(process.env.PORT, () =>{
