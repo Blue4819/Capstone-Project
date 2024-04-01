f0c2e324fb0098fe93b5cbd8145dcb6d0ad98b92
Prachi Bharti <prachi.bharti21@st.niituniversity.in>
2024-04-01 15:47:49 +0530

    edited userModel, added postModel

--- Server Side/models/userModel.js
+++ Server Side/models/userModel.js
@@ -2,17 +2,25 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
+const UserSchema = new mongoose.Schema(
+    {
+      firstName: {type: String,required: true,min: 2,max: 50,},
+      lastName: {type: String,required: true,min: 2,max: 50,},
+      username: {type:String, required:true, unique:true,},
+      email: {type: String,required: true,max: 50,unique: true,},
+      password: {type: String,required: true,min: 8,},
+      picturePath: {type: String,default: "",},
+      following: {type: Array,default: [],},
+      followers: {type: Array, default: [],},
+      followed_locations: {type: [String], default: []},
+      followed_activities: {type: [String], default: []},
+      location: String,
+      bio: String,
+      gender: String,
+      age: Number,
     },
-
-    email: {
-        type: String,
-        required: true
-    }
-}, {timestamps: true})
+    { timestamps: true }
+  );
 
 module.exports = mongoose.model('User', userSchema)
 
