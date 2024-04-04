e210a04659af6e42d097242149a46b2c821b8f72
Prachi Bharti <prachi.bharti21@st.niituniversity.in>
2024-04-04 11:23:38 +0530

    Added routes and route handlers for user.js

--- Server Side/routes/user.js
+++ Server Side/routes/user.js
@@ -2,21 +2,22 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/userModel')
 
+import {
+    signup,
+    signin,
+    saveInfo,
+    profileInfo,
+  } from "../controllers/userController.js";
+
 //create a new user (will happen during login)
-router.post('/', async (request, response) => {
-    const {username, email} = request.body
-    try{
-        const user = await User.create({username, email})
-        response.status(200).json(user)
-    } catch(error) {
-        response.status(400).json({error: error.message})
-    }
-})
+router.post("/signup", signup);
 
-//
+router.post("/login", signin);
 
-//edit user profile
+router.post("/save_info", saveInfo);
 
-//delete user profile
+router.get("/:id", profileInfo);
+
+router.get("/:id", ownProfileInfo);
 
 module.exports = router
