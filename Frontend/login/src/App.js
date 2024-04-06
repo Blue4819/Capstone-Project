6f2808b359f141fa00ea25fe0928dfc97450e1ba
Aanya Tripathi <aanyatripathi2003@gmail.com>
2024-04-06 19:57:33 +0530

    created login page

---
+++ Frontend/login/src/App.js
@@ -0,0 +1,13 @@
+import HolidateLogin from './Components/Holidate Login /HolidateLogin';
+import Header from "./Components/Assets/Header/header.jsx";
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <HolidateLogin />
+      </div>
+  );
+}
+
+export default App;
