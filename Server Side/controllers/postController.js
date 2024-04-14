56e87efd286f410b88182d3a91ba9b61812c7358
Prachi Bharti <prachi.bharti21@st.niituniversity.in>
2024-04-14 23:36:44 +0530

    Added more functions in Post

--- Server Side/controllers/postController.js
+++ Server Side/controllers/postController.js
@@ -21,3 +21,58 @@ export const seePost = async (req,res) => {
     res.json({ error });
   }
 }
+
+export const getUserPosts = async(req,res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
+
+export const likePost = async(req,res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
+
+export const addComment = async(req,res) => {
+  const { id } = req.params;
+  const { userId, comment } = req.body;
+
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    post.comments.push({ userId, comment });
+    await post.save();
+
+    res.status(201).json({ message: "Comment added successfully", post });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+}
--- Server Side/routes/post.js
+++ Server Side/routes/post.js
@@ -4,10 +4,19 @@ const Post = require('../models/postModel')
 
 import{
     newPost,
-    seePost
+    seePost,
+    getUserPosts,
+    likePost,
+    addComment
 } from "../controllers/postController.js";
 
 //go to specific post
 router.post("/new", newPost);
 
 router.get("/:id", seePost);
+
+router.get("/:userId/posts", getUserPosts);
+
+router.patch("/:id/like", likePost);
+
+router.post('/:id/comments', addComment);
