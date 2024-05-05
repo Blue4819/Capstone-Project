697a833ea04fa06c6727570ecdd5aec509fd6b18
Aanya Tripathi <aanyatripathi2003@gmail.com>
2024-05-06 00:45:51 +0530

    SideBar Fixed a bit and dashboard

--- frontend/src/pages/dashboard/dashboard.js
+++ frontend/src/pages/dashboard/dashboard.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import './dashboard.css'
-import Sidebar from '../SideBarSection/sidebar'
-import Body from './Components/BodySection/Body'
+import React from 'react';
+import './dashboard.css';
+import Sidebar from '../SideBarSection/sidebar';
+import Body from './Components/BodySection/Body';
 
 const Dashboard = () => {
-    return(
-        <div className='container'>
-            <Sidebar/>
-            <Body/>
-
+    return (
+        <div className='dashboard-container'>
+            <div className='sidebar'>
+                <Sidebar/>
+            </div>
+            <div className='container'>
+                <Body/>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Dashboard
+export default Dashboard;
+
