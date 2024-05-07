5ec43b70bce280ce4284d77ea5427891b01b4d24
Aanya Tripathi <aanyatripathi2003@gmail.com>
2024-05-07 23:50:56 +0530

    Fixed the Profile page css and code

--- frontend/src/pages/ViewProfile/profile.js
+++ frontend/src/pages/ViewProfile/profile.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './viewprofile.css'; // Import CSS file
 import Sidebar from '../SideBarSection/sidebar';
-
-
+import img from '../ViewProfile/index.jpg';
 export const Profile = () => {
   const decoded = JSON.parse(localStorage.getItem('auth'));
 
@@ -20,42 +19,77 @@ export const Profile = () => {
   }
 
   return (
-    <div className="container">
-      <h1 id="userName">Profile Page</h1>
-      <div className="profile-pic-container">
-        <img id="profilePicPreview" className="profile-pic" src={base64String ? `data:${picturePath.type};base64,${base64String}` : '/path/to/default-profile-picture.jpg'} alt="Profile Picture Preview" />
+    <div className="vprofile_container">
+      <div className='side'>
+        <Sidebar/>
       </div>
-      <div className="user-details">
-        <div className="detail">
-          <label htmlFor="name">Name:</label>
-          <span id="name">{name}</span>
+      <div className='profile'>
+        <h1 id="userName">Profile Page</h1>
+        <div className="profile-pic-container">
+          <img id="profilePicPreview" className="profile-pic" src={base64String ? `data:${picturePath.type};base64,${base64String}` : '/path/to/default-profile-picture.jpg'} alt="Profile Picture Preview" />
         </div>
-        <div className="detail">
-          <label htmlFor="age">Age:</label>
-          <span id="age">{age}</span>
+        <div className="user-details">
+          <div className="detail">
+            
+            <label htmlFor="name">Name:</label>
+            <span id="name">{name}</span>
+          </div>
+          <div className="detail">
+            <label htmlFor="age">Age:</label>
+            <span id="age">{age}</span>
+          </div>
+          <div className="detail">
+            <label htmlFor="gender">Gender:</label>
+            <span id="gender">{gender}</span>
+          </div>
         </div>
-        <div className="detail">
-          <label htmlFor="gender">Gender:</label>
-          <span id="gender">{gender}</span>
+        <hr className="separator" />
+        
+        
+        <div className="interests">
+          
+          <label>Interests:</label>
+          <ul id="interestsList">
+            {interests.map((interest, index) => (
+              <li key={index}>{interest}</li>
+            ))}
+          </ul>
         </div>
-      </div>
-      <hr className="separator" />
-      <div className="interests">
-        <label>Interests:</label>
-        <ul id="interestsList">
-          {interests.map((interest, index) => (
-            <li key={index}>{interest}</li>
-          ))}
-        </ul>
-      </div>
-      <hr className="separator" />
-      <div className="locations">
-        <label htmlFor="locationInput">Visited Places:</label>
-        <ul id="locationsList">
-          {locations.map((location, index) => (
-            <li key={index}>{location}</li>
-          ))}
-        </ul>
+        <hr className="separator" />
+        <div className="locations">
+          
+          <label htmlFor="locationInput">Visited Places:</label>
+          <ul id="locationsList">
+            {locations.map((location, index) => (
+              <li key={index}>{location}</li>
+            ))}
+          </ul>
+        </div>
+        <hr className="separator" />
+        <div className="posts-container">
+          {/* Sample posts */}
+          <div className="post">
+            <div className="post-details">
+              <p className="post-description">Sample post 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <img className="post-image" src={img} alt="Post Image" />
+              <p className="post-location">Location 1</p>
+            </div>
+          </div>
+          <div className="post">
+            <div className="post-details">
+              <p className="post-description">Sample post 2. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+              <img className="post-image" src="index.jpg" alt="Post Image" />
+              <p className="post-location">Location 2</p>
+            </div>
+          </div>
+          <div className="post">
+            <div className="post-details">
+              <p className="post-description">Sample post 3. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <img className="post-image" src="index.jpg" alt="Post Image" />
+              <p className="post-location">Location 3</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
