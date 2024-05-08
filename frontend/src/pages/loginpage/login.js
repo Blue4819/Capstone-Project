a7edbb7aef6c689d1d2d502b526fe3febc9f9bd2
Prachi Bharti <prachi.bharti21@st.niituniversity.in>
2024-05-08 12:12:26 +0530

    Updated login.js

--- frontend/src/pages/loginpage/login.js
+++ frontend/src/pages/loginpage/login.js
@@ -1,18 +1,16 @@
 import Form from "./form";
-import logoImage from "../photos/Group 14.png"; 
+import logoImage from "../photos/Group 14.png";
 import RightImage from "../photos/imageligin.jpg";
 import RightImage2 from "../photos/light.png";
 import RightImage3 from "../photos/Rectangle 35.jpg";
 import RightImage4 from "../photos/img2.png";
-
-
 const LoginPage = () => {
   return (
     <div>
       {/* Header */}
       <header className="bg-[#EBDFCE] py-4 px-9 flex items-center justify-between rounded-full">
         {/* Logo */}
-        <img src={logoImage} alt="Holidate" className="h-20 rounded-xl" />
+        <img src={logoImage} alt="Holidate" className="h-20 rounded-xl mr-4" />
         {/* Navigation */}
         <nav className="flex items-center text-semi-bold text-lg">
           <a href="/signup" className="hover:underline">Sign Up!</a>
@@ -20,11 +18,7 @@ const LoginPage = () => {
       </header>
       {/* Main Content */}
       <div className="flex w-full h-screen">
-        <div></div>
-        <div className="w-full flex items-center justify-center lg:w-1/2">
-          <Form />
-        </div>
-        <div className="hidden relative lg:flex flex-col justify-center items-center w-1/2 bg-[#FFF6ED] rounded-lg p-4">
+        <div className="w-1/2 bg-[#FFF6ED] rounded-lg p-4 mt-8">
           {/* Text Content */}
           <h1 className="text-4xl text-gray-800 mb-4">Connect to new people and places!</h1>
           {/* Image Grid */}
@@ -47,6 +41,9 @@ const LoginPage = () => {
           </div>
           {/* More Text */}
           <h1 className="text-3xl text-gray-600 mt-4"><bold>See and share what you're up to!</bold></h1>
+        </div>
+        <div className="w-1/2 flex items-center justify-center">
+          <Form className="mt-16 mb-8" />
         </div>
       </div>
     </div>
